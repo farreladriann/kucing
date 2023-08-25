8edbd74e53ffc5423100c6972f92d28af180f5df
Farepal <repalfarel@gmail.com>
2023-08-25 13:22:04 +0700

    fff

--- cs/Kucing/Program.cs
+++ cs/Kucing/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string greeting = "      Hello World!       ";
+// Console.WriteLine($"[{greeting}]");
+
+// string trimmedGreeting = greeting.TrimStart();
+// Console.WriteLine($"[{trimmedGreeting}]");
+
+// trimmedGreeting = greeting.TrimEnd();
+// Console.WriteLine($"[{trimmedGreeting}]");
+
+// trimmedGreeting = greeting.Trim();
+// Console.WriteLine($"[{trimmedGreeting}]");
+//=============
+// string sayHello = "Hello World!";
+// Console.WriteLine(sayHello);
+// sayHello = sayHello.Replace("Hello", "Greetings");
+// Console.WriteLine(sayHello)
+//========
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace helloWorld
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Kontolk");
+        }
+    }
+}
