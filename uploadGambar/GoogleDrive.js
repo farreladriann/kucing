e5117eb257e27e6ed470b69ad2e4005d5c35bcf9
Farepal <repalfarel@gmail.com>
2023-11-08 16:16:38 +0700

    gas

--- uploadGambar/GoogleDrive.js
+++ uploadGambar/GoogleDrive.js
@@ -1,9 +1,9 @@
 const { google } = require('googleapis')
 
 const OAuth2Client = new google.auth.OAuth2(
-    '835166406485-1mbmb7hlj4f68t9jnpsbgd1sao1v8qa9.apps.googleusercontent.com',
-    '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',
-    'https://developers.google.com/oauthplayground'
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
+    GOOGLE_REDIRECT_URI
 )
 
 OAuth2Client.setCredentials({ refresh_token: '1//04p2supXQLTW0CgYIARAAGAQSNwF-L9IrZbBtmxDYAyk3WQE5dyrfpAx546A0j3r7LfLWbXe08GY6zKwlAhrDLvle6YEJ7auqpvM' })
