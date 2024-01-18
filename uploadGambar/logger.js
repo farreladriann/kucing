788b28fd698e3a3091c2133d2c74a348334b07bb
Farepal <repalfarel@gmail.com>
2024-01-19 01:05:43 +0700

    gas

---
+++ uploadGambar/logger.js
@@ -0,0 +1,17 @@
+const pino = require('pino')
+
+export default pino({
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
+    transport: {
+      targets: [
+        {
+          target: "pino/file",
+          level: "error",
+          options: { destination: "./logs/app.log", mkdir: true },
+        },
+        {
+          target: "pino-pretty",
+        },
+      ],
+    },
+  })
